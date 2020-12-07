5d725fca3aeafd7015bffda996c5aa06ee29e267
Mario Campopiano <mcampopiano92@gmail.com>
2020-12-07 17:45:19 -0600

    setup directory files for magazines, created magazine provider module

---
+++ src/components/magazines/MagazineProvider.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react"
+
+export const MagazineContext = React.createContext()
+
+export const MagazineProvider = (props) => {
+    const [magazines, setMagazines] = useState([])
+
+    const getMagazines = () => {
+        return fetch("http://localhost:8088/magazines")
+        .then(r => r.json())
+        .then(setMagazines)
+    }
+
+    const addMagazine = (magazine) => {
+        return fetch("http://localhost:8088/magazines", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(magazine)
+        })
+        .then(getMagazines)
+    }
+
+    return (
+        <MagazineContext.Provider value={{magazines, setMagazines, getMagazines, addMagazine}}>
+            {props.children}
+        </MagazineContext.Provider>
+    )
+}
