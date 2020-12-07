41d13f7f2505d23411e8a7dd74ef022631d25985
Mario Campopiano <mcampopiano92@gmail.com>
2020-12-07 13:36:00 -0600

    Installed log in and registration capabilities

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import { POP } from './App';
 
 ReactDOM.render(
+
   <React.StrictMode>
-    <App />
+    <Router>
+      <POP />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
