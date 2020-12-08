4e46e0c9b3699680fd758f230b28cb1ba9a126de
Mario Campopiano <mcampopiano92@gmail.com>
2020-12-08 10:59:15 -0600

    Added a checkbox in each story card to be marked to show complete or not

--- src/components/stories/Story.js
+++ src/components/stories/Story.js
@@ -1,10 +1,19 @@
-import React from "react"
+import React, { useContext, useRef } from "react"
 import { Link } from "react-router-dom"
+import { StoryContext } from "./StoryProvider"
 
 export const Story = ({ story, history }) => {
+    const {editStory} = useContext(StoryContext)
+    const complete = useRef(null)
+    const checked = () => {
+        console.log(complete.current.checked)
+    }
+
     return (
         <div className="story">
             <h3>{story.title}</h3>
+            <label htmlFor="completeBox">Check when completed</label>
+            <input type="checkbox" ref={complete} onChange={checked}/>
             <p>Total word count goal: {story.totalWordGoal}</p>
             <p>Daily word count goal: {story.dailyWordGoal}</p>
             <button onClick={() => history.push(`/stories/notes/create/${story.id}`)}>Add note</button>
