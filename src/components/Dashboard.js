dff35da5d61f7c9e1b2131e2d20b41ba795d6ec9
Mario Campopiano <mcampopiano92@gmail.com>
2020-12-07 18:41:41 -0600

    Added Publish Or Perish to the dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,6 +5,7 @@ import { StoryList } from "./stories/StoryList"
 export const Dashboard = (props) => {
     return (
         <>
+            <h1>Publish Or Perish</h1>
             <MagazineList {...props}/>
             <StoryList {...props}/>
         </>
