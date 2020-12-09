e37659d493ed01ff730be2cb5816cd79874379f0
Mario Campopiano <mcampopiano92@gmail.com>
2020-12-08 18:50:13 -0600

    The submit form now creates a new object in the JSON server join table for submitted stories.

--- src/components/submittedStories/SubmittedStoriesForm.js
+++ src/components/submittedStories/SubmittedStoriesForm.js
@@ -7,19 +7,20 @@ export const SubmissionForm = (props) => {
     const {addSubmission} = useContext(SubmittedStoriesContext)
     const {getStories, stories} = useContext(StoryContext)
 
-    const story = useRef(null)
+    const storyId = useRef(null)
     const dateSubmitted = useRef(null)
     const response = useRef(null)
 
     const constructSubmission = () => {
-        console.log({
+        addSubmission({
             magazineId: parseInt(magazine.id),
-            storyId: parseInt(story.id),
+            storyId: parseInt(storyId.current.value),
             dateSubmitted: dateSubmitted.current.value,
             expectedResponseDate: response.current.value,
             isPending: true,
             accepted: false
         })
+        .then(props.history.push(`/magazines/${magazine.id}`))
     }
 
     useEffect(() => {
@@ -31,7 +32,7 @@ export const SubmissionForm = (props) => {
         <form className="submissionForm">
             <fieldset>
                 <div className="form-group">
-                    <select defaultValue="" ref={story} className="form-control">
+                    <select defaultValue="" ref={storyId} className="form-control">
                         <option value="0">Select a story to submit</option>
                         {
                             stories.map(story => {
@@ -43,6 +44,24 @@ export const SubmissionForm = (props) => {
                     </select>
                 </div>
             </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="dateSubmitted">Select date submitted: </label>
+                    <input type="date" id="dateSubmitted" ref={dateSubmitted}/>
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="response">Select date of expected response: </label>
+                    <input type="date" id="response" ref={response}/>
+                </div>
+            </fieldset>
+            <button type="submit" onClick={event => {
+                event.preventDefault()
+                constructSubmission()
+            }}>
+                Save submission
+            </button>
         </form>
     )
 }
