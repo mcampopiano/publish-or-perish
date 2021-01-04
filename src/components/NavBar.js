451ae8f750b6d8ed74ac2688b9637830d84e8728
Mario Campopiano <mcampopiano92@gmail.com>
2021-01-04 13:12:10 -0600

    Added a homepage Icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,15 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 import "./NavBar.css"
 
 export const NavBar = (props) => {
+    const element = <FontAwesomeIcon icon={faHome} />
     return (
         <ul className="navbar">
             <li className="navbar__item active">
-                <Link className="navbar__link" to="/">Home</Link>
+                <Link className="navbar__link" to="/">{element}</Link>
             </li>
             <li className="navbar__item">
                 <Link className="navbar__link" to="/stories/complete">Completed stories</Link>
